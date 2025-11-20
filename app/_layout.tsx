fef8392480e0ce170243b535a1acd874cc2e6d3e
yanki <yankiyyk@gmail.com>
2025-11-21 02:23:06 +0800

    Testing tanstack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,20 @@
 import { Stack } from "expo-router";
 import "../global.css";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+
+// Create a client
+const queryClient = new QueryClient()
+
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+
+          <QueryClientProvider client={queryClient}>
+            <Stack />
+          </QueryClientProvider>
+          )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,27 @@
-import { Text, View } from "react-native";
+import {
+  useQuery
+} from '@tanstack/react-query';
+import { View } from "react-native";
 
 export default function Index() {
+            const { isPending, error, data } = useQuery({
+              queryKey: ['repoData'],
+              queryFn: () =>
+                fetch('https://api.github.com/repos/TanStack/query').then((res) =>
+                  res.json(),
+                ),
+            })
+
+            if (isPending) return 'Loading...'
+
+            if (error) return 'An error has occurred: ' + error.message
+
   return (
+
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
+            <h1>{data.name}</h1>
+            <p>{data.description}</p>
+
     </View>
   );
 }
